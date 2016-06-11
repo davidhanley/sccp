9db25402f683562971bdacfd12c284f315f84cef
David Hanley <dhanley@groupon.com>
2016-06-11 07:22:41 -0500

    tightened up the code a lot

--- src/sccp/core.clj
+++ src/sccp/core.clj
@@ -1,12 +1,53 @@
 (ns sccp.core
   (:gen-class))
+
 (use '[clojure.set :only (map-invert)])
 
 (defn l [] (use 'sccp.core :reload))
 
-(def squares (range 0 64))
+; the indices for all the chess squares
+(def square-indices (range 0 64))
 
-(def squares-to-coords (zipmap squares (map #(hash-map :r (quot % 8) :f (mod % 8)) squares)))
-(def coords-to-squares (map-invert squares-to-coords))
+;the coordinates for all the chess squares 
+(def square-coordinates (map #(hash-map :r (quot % 8) :f (mod % 8)) square-indices))
+
+;make the symbols for the chess squares
+(def files (vec "abcdefgh"))
+(def ranks (vec "87654321"))
+(def square-strings (map #(str (files (:f %))(ranks (:r %))) square-coordinates)) 
+
+;and now the symbols 
+(def square-symbols (map keyword square-strings))
+
+; mapping from coordinates to squares
+(def coord-to-square (zipmap square-coordinates square-symbols))
+(def square-to-coord (map-invert coord-to-square))
 
 (def add-coords (partial merge-with +))
+
+(def to-coords (partial map (fn[[r f]]{:r r :f f})))
+
+(def knight-deltas [[-2 -1][-2 1][-1 -2][-1 2][1 -2][1 2][2 -1][2 1]])
+(def king-deltas [[-1 -1][-1 0][-1 1][0 -1][0 1][1 -1][1 0][1 1]])
+
+(defn make-move[f t]{:f (coord-to-square f) :t (coord-to-square t)})
+
+(defn hopper-gen[delt] 
+  (into {} (map (fn [[c sq]][sq (map (partial make-move c) (filter coord-to-square (map (partial add-coords c) (to-coords delt))))]) coord-to-square)))
+
+(def knight-moves (hopper-gen knight-deltas))
+
+(def king-moves (hopper-gen king-deltas))
+
+(defn ray[s d](map (partial make-move s) (rest (take-while coord-to-square (iterate (partial add-coords d) s)))))
+
+(def rook-deltas [[1 0][-1 0][0 1][0 -1]])
+(def bishop-deltas [[-1 -1][-1 1][1 -1][1 1]])
+
+(defn moves-for-slider[deltas square](filter not-empty (map (partial ray square) (to-coords deltas))))
+
+(defn slider-gen[deltas] (into {} (map (fn[[c sq]][sq (moves-for-slider deltas c)]) coord-to-square)))
+
+(def rook-moves (slider-gen rook-deltas))
+(def bishop-moves (slider-gen bishop-deltas))
+(def queen-moves (merge-with concat rook-moves bishop-moves))
--- test/sccp/core_test.clj
+++ test/sccp/core_test.clj
@@ -4,20 +4,33 @@
 
 (deftest square-test
   (testing "basic chess squares stats"
-    (is (= (count squares) 64)))
-  (testing "square to coords"
-    (is (= (count squares-to-coords) 64))
-    (is (= (squares-to-coords 4) {:r 0, :f 4}))
-    (is (= (squares-to-coords 444) nil)))
+    (is (= (count square-indices) 64)))
   (testing "coords to squares"
-    (is (= (coords-to-squares {:r 1 :f 1}) 9))
-    (is (= (coords-to-squares {:r 1 :f 1}) 9)) 
-    (is (= (coords-to-squares {:r 9 :f 1}) nil)
-    (is (= (coords-to-squares {:r 4 :f 6}) 38))))
+    (is (= (coord-to-square {:r 1 :f 1}) :b7)) 
+    (is (= (coord-to-square {:r 9 :f 1}) nil)
+    (is (= (coord-to-square {:r 4 :f 6}) :g4))))
   (testing "square math"
     (is (= (add-coords {:r 3 :f 4} {:r 1 :f 1}) {:r 4 :f 5}))
-    (is (= (add-coords {:r 3 :f 4} {:r 0 :f -1}) {:r 3 :f 3})))
+    (is (= (add-coords {:r 3 :f 4} {:r 0 :f -1}) {:r 3 :f 3})))  	     
            
 )
 
+(deftest basic-movegen
+  (testing "ray casting"
+    (is (= (count (moves-for-slider rook-deltas {:r 0 :f 0})) 2))
+    (is (= (count (moves-for-slider bishop-deltas {:r 0 :f 0})) 1)))
+)
+
+(deftest move-lookups 
+  (testing "knight move counts"
+    (is (= (count (knight-moves :e4)) 8))
+    (is (= (count (knight-moves :h1)) 2)))
+  (testing "king-move-counts"
+    (is (= (count (king-moves :e4)) 8))
+    (is (= (count (king-moves :e1)) 5))
+    (is (= (count (king-moves :h1)) 3)))
+  (testing "bishop-move-counts"
+    (is (= (count (reduce concat (bishop-moves :h1))) 7))
+    (is (= (count (reduce concat (bishop-moves :e4))) 13)))
+)
 
